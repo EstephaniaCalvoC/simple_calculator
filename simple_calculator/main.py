9440af7b44db4fd845ca2d2ad10f37ce2972422c
estephaniacalvoc <estephania.calvo.c@gmail.com>
2021-11-23 14:49:21 +0000

    Part 1 - Introduction and TDD rules

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,1 +1,18 @@
 # -*- coding: utf-8 -*-
+from functools import reduce
+
+
+class SimpleCalculator:
+    def add(self, *args):
+        return sum(args)
+    
+    def sub(self, a, b):
+        return a - b
+    
+    def mul(self, *args):
+        if not all(args):
+            raise ValueError
+        return reduce(lambda x, y: x*y, args)
+    
+    def div(self, a, b):
+        return a / b if b else float('inf')
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,80 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from simple_calculator.main import SimpleCalculator
+import pytest
+
+
+def test_add_two_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.add(4, 5)
+
+    assert result == 9
+
+    
+def test_add_three_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.add(4, 5, 6)
+
+    assert result == 15
+
+    
+def test_add_many_numbers():
+    numbers = range(100)
+
+    calculator = SimpleCalculator()
+
+    result = calculator.add(*numbers)
+
+    assert result == 4950
+    
+    
+def test_subtract_two_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.sub(10, 3)
+
+    assert result == 7
+    
+    
+def test_mul_two_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.mul(6, 4)
+
+    assert result == 24
+
+
+def test_mul_many_numbers():
+    numbers = range(1, 10)
+
+    calculator = SimpleCalculator()
+
+    result = calculator.mul(*numbers)
+
+    assert result == 362880
+
+    
+def test_div_two_numbers_float():
+    calculator = SimpleCalculator()
+
+    result = calculator.div(13, 2)
+
+    assert result == 6.5
+    
+    
+def test_div_by_zero_returns_inf():
+    calculator = SimpleCalculator()
+
+    result = calculator.div(5, 0)
+
+    assert result == float('inf')
+
+    
+def test_mul_by_zero_raises_exception():
+    calculator = SimpleCalculator()
+
+    with pytest.raises(ValueError):
+        calculator.mul(3, 0)
